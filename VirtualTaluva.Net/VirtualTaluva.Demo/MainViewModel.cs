1be6993caef61f12a2096c6c99cad656e77a1b68
Éric Massé <Ericmas001@gmail.com>
2017-01-22 17:17:55 -0500

    Scrolling & Scaling

---
+++ VirtualTaluva.Net/VirtualTaluva.Demo/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Com.Ericmas001.Windows;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace VirtualTaluva.Demo
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private double m_Scale = 1;
+
+        [SuppressMessage("ReSharper", "ExplicitCallerInfoArgument")]
+        public double Scale
+        {
+            get { return m_Scale; }
+            set
+            {
+                Set(ref m_Scale, value);
+                RaisePropertyChanged(nameof(ZoomValue));
+            }
+        }
+        public string ZoomValue => $"{m_Scale*100:0}%";
+
+        private RelayCommand m_ZoomInCommand;
+        public RelayCommand ZoomInCommand => m_ZoomInCommand ?? (m_ZoomInCommand = new RelayCommand(OnZoomIn, CanZoomIn));
+
+        private RelayCommand m_ZoomOutCommand;
+        public RelayCommand ZoomOutCommand => m_ZoomOutCommand ?? (m_ZoomOutCommand = new RelayCommand(OnZoomOut, CanZoomOut));
+        
+
+        private void OnZoomIn()
+        {
+            Scale *= 1.25;
+        }
+        private bool CanZoomIn()
+        {
+            return Scale < 5.96;
+        }
+        private void OnZoomOut()
+        {
+            Scale /= 1.25;
+        }
+        private bool CanZoomOut()
+        {
+            return Scale > 0.33;
+        }
+    }
+}
--- VirtualTaluva.Net/VirtualTaluva.Demo/MainWindow.xaml.cs
+++ VirtualTaluva.Net/VirtualTaluva.Demo/MainWindow.xaml.cs
@@ -18,27 +18,11 @@ namespace VirtualTaluva.Demo
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-            //var p = new Polygon
-            //{
-            //    Points = new PointCollection(new Point[]
-            //    {
-            //        new Point(45, 0),
-            //        new Point(90, 35),
-            //        new Point(90, 84.5),
-            //        new Point(45, 119.5),
-            //        new Point(0, 84.5),
-            //        new Point(0, 35),
-            //    }),
-            //    Stroke = Brushes.Black,
-            //    StrokeThickness = 1,
-            //    Margin = new Thickness(10)
-            //};
-            //MyGrid.Children.Add(p);
         }
     }
 }
