dc61ab4cf678967ae353a1d407517813dd1ed047
Éric Massé <Ericmas001@gmail.com>
2017-01-22 15:24:33 -0500

    First Commit

---
+++ VirtualTaluva.Net/VirtualTaluva.Demo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VirtualTaluva.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //var p = new Polygon
+            //{
+            //    Points = new PointCollection(new Point[]
+            //    {
+            //        new Point(45, 0),
+            //        new Point(90, 35),
+            //        new Point(90, 84.5),
+            //        new Point(45, 119.5),
+            //        new Point(0, 84.5),
+            //        new Point(0, 35),
+            //    }),
+            //    Stroke = Brushes.Black,
+            //    StrokeThickness = 1,
+            //    Margin = new Thickness(10)
+            //};
+            //MyGrid.Children.Add(p);
+        }
+    }
+}
