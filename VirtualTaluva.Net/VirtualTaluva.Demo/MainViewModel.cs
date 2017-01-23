0428d9d60dc38f93db5517f87af201aef286c159
Éric Massé <Ericmas001@gmail.com>
2017-01-22 19:58:32 -0500

    Can Rotate

--- VirtualTaluva.Net/VirtualTaluva.Demo/MainViewModel.cs
+++ VirtualTaluva.Net/VirtualTaluva.Demo/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Windows;
 using Com.Ericmas001.Windows;
 using GalaSoft.MvvmLight.CommandWpf;
 
@@ -7,7 +9,21 @@ namespace VirtualTaluva.Demo
 {
     public class MainViewModel : BaseViewModel
     {
+        private static readonly Thickness m_BaseMargin = new Thickness(173.20508, 70, 0, 0);
+
+        private static readonly Dictionary<double, Thickness> m_RotationMarginModifier = new Dictionary<double, Thickness>
+        {
+            {0, new Thickness(0)},
+            {60, new Thickness(-26.5,-15,0,0)},
+            {120, new Thickness(7.5,15,0,0)},
+            {180, new Thickness(-35.5,-1.5,0,0)},
+            {240, new Thickness(-8.75,13.5,0,0)},
+            {300, new Thickness(-43,-15.5,0,0)},
+        };
+
         private double m_Scale = 1;
+        private double m_Angle = 0;
+        private Thickness m_CurrentMargin= new Thickness(m_BaseMargin.Left, m_BaseMargin.Top, m_BaseMargin.Right, m_BaseMargin.Bottom);
 
         [SuppressMessage("ReSharper", "ExplicitCallerInfoArgument")]
         public double Scale
@@ -19,6 +35,18 @@ namespace VirtualTaluva.Demo
                 RaisePropertyChanged(nameof(ZoomValue));
             }
         }
+
+        public double RotateAngle
+        {
+            get { return m_Angle; }
+            set { Set(ref m_Angle, value); }
+        }
+
+        public Thickness CurrentMargin
+        {
+            get { return m_CurrentMargin; }
+            set { Set(ref m_CurrentMargin, value); }
+        }
         public string ZoomValue => $"{m_Scale*100:0}%";
 
         private RelayCommand m_ZoomInCommand;
@@ -26,7 +54,17 @@ namespace VirtualTaluva.Demo
 
         private RelayCommand m_ZoomOutCommand;
         public RelayCommand ZoomOutCommand => m_ZoomOutCommand ?? (m_ZoomOutCommand = new RelayCommand(OnZoomOut, CanZoomOut));
-        
+
+        private RelayCommand m_RotateCommand;
+        public RelayCommand RotateCommand => m_RotateCommand ?? (m_RotateCommand = new RelayCommand(OnRotate));
+
+        private RelayCommand m_AntiRotateCommand;
+        public RelayCommand AntiRotateCommand => m_AntiRotateCommand ?? (m_AntiRotateCommand = new RelayCommand(OnAntiRotate));
+
+        public MainViewModel()
+        {
+            RecalculateMargin();
+        }
 
         private void OnZoomIn()
         {
@@ -43,6 +81,23 @@ namespace VirtualTaluva.Demo
         private bool CanZoomOut()
         {
             return Scale > 0.33;
+        }
+
+        private void OnRotate()
+        {
+            RotateAngle = (RotateAngle + 60) % 360;
+            RecalculateMargin();
+        }
+        private void OnAntiRotate()
+        {
+            RotateAngle = (RotateAngle + 300) % 360;
+            RecalculateMargin();
+        }
+
+        private void RecalculateMargin()
+        {
+            var rotationModifier = m_RotationMarginModifier.ContainsKey(RotateAngle) ? m_RotationMarginModifier[RotateAngle] : new Thickness(0);
+            CurrentMargin = new Thickness(m_BaseMargin.Left + rotationModifier.Left, m_BaseMargin.Top + rotationModifier.Top, m_BaseMargin.Right + rotationModifier.Right, m_BaseMargin.Bottom + rotationModifier.Bottom);
         }
     }
 }
