44e443c80d7b54c783903d7da431bcd502e4656f
Éric Massé <Ericmas001@gmail.com>
2017-01-23 21:56:38 -0500

    HLSL compile help

--- VirtualTaluva.Net/VirtualTaluva.Demo/Resources/AngularGradientEffect.hlsl
+++ VirtualTaluva.Net/VirtualTaluva.Demo/Resources/AngularGradientEffect.hlsl
@@ -4,6 +4,9 @@ float4 firstColor : register(C1);
 float4 secondColor : register(C2);
 float4 thirdColor : register(C3);
 
+//To compile
+//"C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Utilities\bin\x86\fxc.exe" /T ps_3_0 /Fo "D:\Programmation\VirtualTaluva\VirtualTaluva.Net\VirtualTaluva.Demo\Resources\AngularGradientEffect.ps"  "D:\Programmation\VirtualTaluva\VirtualTaluva.Net\VirtualTaluva.Demo\Resources\AngularGradientEffect.hlsl"
+
 float4 main(float2 uv : TEXCOORD) : COLOR
 {
     clip(tex2D(inputSampler, uv).a - 0.01);
